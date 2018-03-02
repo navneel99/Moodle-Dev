aa9cc8b2d38a89f1a7df81382a7cd239b6ed83f1
navneel99 <34075042+navneel99@users.noreply.github.com>
2018-03-02 18:39:36 +0530

    Create rootpass.php
    Extra include file. Add username and password of MySQL to run.

---
+++ rootpass.php
@@ -0,0 +1,11 @@
+<?php
+
+$user='';
+$pass=''; 
+
+?>
+
+##############################
+
+/* Here add the rootusername of mysql in your system in the quotes of $user
+and add the password ofyour mysql root in the quotes of $pass  */
